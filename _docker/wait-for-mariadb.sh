6062784f8ba164c706dd87c27a3deeb8e0024c93
vitaly <vitalicheg28@mail.ru>
2025-03-14 14:10:24 +0100

    add bookings-service to docker-compose.yml

---
+++ _docker/wait-for-mariadb.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Проверяем, доступна ли база данных MariaDB
+until mysql -h mariadb -u root -ppassword -e "SELECT 1"; do
+  echo "Waiting for MariaDB to be available..."
+  sleep 2
+done
+
+# После того как база данных доступна, запускаем Spring Boot приложение
+java -jar /path/to/your/bookings-service.jar
