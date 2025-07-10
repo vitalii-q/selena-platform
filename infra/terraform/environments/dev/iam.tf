0b38e10fc5608b86dc207d17de19ec40de223f12
vitaly <vitalicheg28@mail.ru>
2025-07-10 17:40:26 +0200

    delete iam role duplicate

--- infra/terraform/environments/dev/iam.tf
+++ infra/terraform/environments/dev/iam.tf
@@ -20,6 +20,28 @@ resource "aws_iam_instance_profile" "cloudwatch_agent_instance_profile" {
   role = aws_iam_role.cloudwatch_agent_server_role.name
 }
 
+resource "aws_iam_role" "cloudwatch_agent_server_role" {
+  name = "cloudwatch-agent-server-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+
+    tags = {
+    Project = "Selena"
+    Service = "users-service"
+  }
+}
+
 # Создаём terraform-user
 resource "aws_iam_user" "terraform_user" {
   name = "terraform-user"
@@ -43,6 +65,13 @@ resource "aws_iam_user_policy" "terraform_user_policy" {
           "iam:PassRole",
           "iam:CreateInstanceProfile",
           "iam:AddRoleToInstanceProfile"
+        ],
+        Resource = "*"
+      },
+      {
+        Effect = "Allow",
+        Action = [
+          "ec2:Describe*"
         ],
         Resource = "*"
       }
