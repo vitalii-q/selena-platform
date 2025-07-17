775998f2d98acdbdc73508726813119f9e846bef
vitaly <vitalicheg28@mail.ru>
2025-07-17 17:59:15 +0200

    add depends_on to aws_eip

--- infra/terraform/modules/ec2/main.tf
+++ infra/terraform/modules/ec2/main.tf
@@ -133,6 +133,10 @@ data "aws_ami" "amazon_linux_2023" {
 resource "aws_eip" "this" {
   instance = aws_instance.users_service[0].id
 
+    depends_on = [
+      aws_instance.users_service[0]
+    ]
+
   lifecycle {
     prevent_destroy = true
   }
