763aa882410e58717e022cd545750306a079a76b
vitaly <vitalicheg28@mail.ru>
2025-08-17 10:40:47 +0200

    refactor: transfer role_policy_attachment

--- infra/terraform/modules/iam/main.tf
+++ infra/terraform/modules/iam/main.tf
@@ -21,11 +21,6 @@ resource "aws_iam_policy" "ec2_stop_start_policy" {
   })
 }
 
-resource "aws_iam_user_policy_attachment" "attach_ec2_policy" {
-  user       = var.user_name
-  policy_arn = aws_iam_policy.ec2_stop_start_policy.arn
-}
-
 resource "aws_iam_role" "ec2_s3_access_role" {
   name = "ec2-s3-access-role"
 
@@ -67,11 +62,6 @@ resource "aws_iam_policy" "ec2_s3_access_policy" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "attach_ec2_s3_access" {
-  role       = aws_iam_role.ec2_s3_access_role.name
-  policy_arn = aws_iam_policy.ec2_s3_access_policy.arn
-}
-
 resource "aws_iam_instance_profile" "ec2_s3_access_profile" {
   name = "ec2-s3-access-profile"
   role = aws_iam_role.ec2_s3_access_role.name
@@ -93,11 +83,6 @@ resource "aws_iam_role" "selena_ec2_role" {
       }
     ]
   })
-}
-
-resource "aws_iam_role_policy_attachment" "attach_s3_policy" {
-  role       = aws_iam_role.selena_ec2_role.name
-  policy_arn = aws_iam_policy.ec2_s3_access_policy.arn
 }
 
 # Создаём instance profile для EC2
