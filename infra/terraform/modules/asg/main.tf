9f78421993d247d39094b57bf600eeb3fe2c3475
vitaly <vitalicheg28@mail.ru>
2025-08-21 09:46:45 +0200

    feat: (particial) add scaling policy

--- infra/terraform/modules/asg/main.tf
+++ infra/terraform/modules/asg/main.tf
@@ -83,20 +83,3 @@ resource "aws_autoscaling_policy" "cpu_tgt_tracking" {
   }
 }
 
-# Scale-Out policy (increase instances when CPU > 70%)
-resource "aws_autoscaling_policy" "scale_out" {
-  name                   = "selena-scale-out"
-  scaling_adjustment     = 1
-  adjustment_type        = "ChangeInCapacity"
-  cooldown               = 300
-  autoscaling_group_name = aws_autoscaling_group.this.name
-}
-
-# Scale-In policy (decrease instances when CPU < 30%)
-resource "aws_autoscaling_policy" "scale_in" {
-  name                   = "selena-scale-in"
-  scaling_adjustment     = -1
-  adjustment_type        = "ChangeInCapacity"
-  cooldown               = 300
-  autoscaling_group_name = aws_autoscaling_group.this.name
-}
